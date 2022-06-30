18d9b3846c5c9050df012f9531890cb6e0f3a9a4
Herdina <gherdina7@gmail.com>
2022-06-30 18:08:38 -0300

    Requisito 18 finalizado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <main>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Play Again
+        </button>
       </main>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
