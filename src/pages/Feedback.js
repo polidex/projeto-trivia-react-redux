b9378743620b6b5a59bc8ad6971bce0a37548b9d
Herdina <gherdina7@gmail.com>
2022-06-30 17:51:09 -0300

    Requisito 14 finalizado

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,48 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import md5 from 'crypto-js/md5';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      hash: '',
-    };
-  }
-
-  componentDidMount() {
-    const { gravatarEmail } = this.props;
-    const hash = md5(gravatarEmail).toString();
-    this.setState({ hash });
-  }
-
   render() {
-    const { hash } = this.state;
-    const { name, score, assertions } = this.props;
-    const number = 3; // magic
+    const { assertions, score } = this.props;
+    const number = 3;
     return (
       <main>
+        <Header />
         <h1>Feedback</h1>
         <div>
-          <img
-            src={ `https://www.gravatar.com/avatar/${hash}` }
-            alt="Player avatar"
-            data-testid="header-profile-picture"
-          />
-          <p data-testid="header-player-name">
-            {' '}
-            {name}
-            {' '}
-          </p>
-          <p
-            data-testid="header-score"
-          >
-            {score}
-          </p>
           <p data-testid="feedback-text">
             {assertions < number ? 'Could be better...' : 'Well Done!'}
+          </p>
+          <p>
+            Você acertou
+            {' '}
+            <span
+              data-testid="feedback-total-question"
+            >
+              {assertions}
+            </span>
+            {' '}
+            questões!
+          </p>
+          <p>
+            Total de
+            {' '}
+            <span data-testid="feedback-total-score">{score}</span>
+            {' '}
+            pontos!
           </p>
         </div>
       </main>
@@ -51,17 +40,13 @@ class Feedback extends React.Component {
 }
 
 Feedback.propTypes = {
-  score: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  gravatarEmail: PropTypes.string.isRequired,
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  name: state.player.name,
-  gravatarEmail: state.player.gravatarEmail,
-  score: state.player.score,
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
